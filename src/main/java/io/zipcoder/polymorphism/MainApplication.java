c924e8461a36b0393c8fa3778661ea8a99abeafa
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-22 15:53:15 -0500

    Lab complete, finished mainApplication with TDD

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,5 +1,8 @@
 package io.zipcoder.polymorphism;
 
+import io.zipcoder.polymorphism.pets.Cat;
+import io.zipcoder.polymorphism.pets.Dog;
+import io.zipcoder.polymorphism.pets.Frog;
 import io.zipcoder.polymorphism.pets.Pets;
 
 import java.util.HashMap;
@@ -8,50 +11,80 @@ import java.util.Scanner;
 
 public class MainApplication {
     Scanner in = new Scanner(System.in);
-    String userName;
+    String userName = "";
+    int numPets = 1;
+    String petName = "";
+    String species = "";
     Pets[] pets;
     Map<String, Pets[]> owners = new HashMap<String, Pets[]>();
 
     public void main(String[] args) {
-        userName = askName();
-        int numPets = howManyPets();
+        getInfo1();
         pets = new Pets[numPets];
         for (int i=0; i<numPets; i++) {
-            String petName = getPetName();
-            String species = getSpecies(petName);
-            pets[i] = createPet(species);
+            getInfo2();
+            pets[i] = createPet(species, petName);
         }
+        owners.put(userName, pets);
         System.out.println(printString());
     }
 
-    public String askName() {
-        return null;
+    public void getInfo1() {
+        System.out.println("What is your name?");
+        userName = in.nextLine();
+        System.out.println("How many pets do you have?");
+        numPets = Integer.parseInt(in.nextLine());
     }
 
+    public void getInfo2() {
+        System.out.println("What is your pet's name?");
+        petName = in.nextLine();
+        System.out.println("What kind of pet is "+petName+"?");
+        petName = in.nextLine();
+    }
 
-    public int howManyPets() {
-        return 0;
+    public String getName() {
+        return userName;
+    }
+
+    public int getHowManyPets() {
+        return numPets;
     }
 
     public String getPetName() {
-        return null;
+        return petName;
     }
 
-    public String getSpecies(String petName) {
-        return null;
+    public String getSpecies() {
+        return species;
     }
 
-    public Pets createPet(String species) {
-        return null;
+    public Pets createPet(String species, String petName) {
+        Pets pet = new Pets();
+        if (species.equalsIgnoreCase("dog")) {
+            pet = new Dog(petName);
+        }
+        else if (species.equalsIgnoreCase("cat")) {
+            pet = new Cat(petName);
+        }
+        else if (species.equalsIgnoreCase("frog")) {
+            pet = new Frog(petName);
+        }
+        else {
+            System.out.println("Species not supported");
+        }
+        return pet;
     }
 
     public String printString() {
-        return null;
-    }
-
-    public enum petType {
-        DOG,
-        CAT,
-        FROG
+        String vocab = "";
+        if (numPets==1) { vocab = "pet"; }
+        else { vocab = "pets"; }
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%s owns %s %s.", userName, numPets, vocab));
+        for (Pets p : pets) {
+            sb.append(String.format("%s is a %s.", p.getPetName(), getSpecies()));
+        }
+        return sb.toString();
     }
 }
--- src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
+++ src/test/java/io/zipcoder/polymorphism/MainApplicationTest.java
@@ -1,5 +1,8 @@
 package io.zipcoder.polymorphism;
 
+import io.zipcoder.polymorphism.pets.Dog;
+import io.zipcoder.polymorphism.pets.Pets;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainApplicationTest {
@@ -7,6 +10,17 @@ public class MainApplicationTest {
     @Test
     public void testMe(){
         MainApplication ma = new MainApplication();
+        String name = ma.getName();
+        int pets = ma.getHowManyPets();
+        String petName = ma.getPetName();
+        Pets dog = ma.createPet("dog", name);
+        Pets cat = ma.createPet("cat", name);
+        Pets frog = ma.createPet("frog", name);
+        Pets pet = ma.createPet("", name);
 
+        Assert.assertEquals("", name);
+        Assert.assertEquals(1, pets);
+        Assert.assertEquals(petName, pet.getPetName());
+        Assert.assertEquals("", ma.getSpecies());
 }
 }
